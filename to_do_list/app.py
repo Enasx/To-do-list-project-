df89fb01a8403b05cfea51563a0260bf82c3e6c9
Enas <enashadad98@gmail.com>
2024-03-03 16:15:47 +0400

    adding separate file to write and read

--- to_do_list/app.py
+++ to_do_list/app.py
@@ -1,47 +1,71 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import json
+
 
 app = FastAPI()
-
 
 
 class Task(BaseModel):
     task_id: int
     task: str
 
-list = [
-    Task(task_id=1, task="Having Breakfast"),
-    Task(task_id=2, task="Going to work"),
-    Task(task_id=3, task="Gym"),
-    Task(task_id=4, task="Buying Groceries"),
-    Task(task_id=5, task="Cooking"),
-    Task(task_id=6, task="Sleeping")
-]
+
+def get_list():
+    with open("list_module.json", "r") as file:
+       list_data = json.load(file)
+    return [Task(**task_data) for task_data in list_data]
+
+def write_tasks(list):
+    list_data = [{"task_id": task.task_id, "task": task.task} for task in list] 
+    with open("list_module.json", "w") as file:
+        json.dump(list_data, file)
 
 @app.get("/list/")
 async def read_list():
-    return list
-
+    return get_list()
 
 @app.post("/add/")
-async def add_task(new_task: Task):
-    new_task_id = len(list) + 1
-    new_task.task_id = new_task_id   
-    list.append(new_task)
-    return new_task
+async def add_task(new_task: str):
+    list = get_list()
+    if len(list) == 0:
+        new_task_id=1
+    else:
+        new_task_id = max(task.task_id for task in list) + 1
+    task_model = Task(task_id=new_task_id, task=new_task)
+    list.append(task_model)
+    write_tasks(list)
+    return task_model
 
-@app.post("/complete/")
+
+""" @app.post("/complete/")
 async def complete_task(task_id: int):
-    if task_id < 0 or task_id >= len(list):
+    list=get_list()
+    if task_id < 0 or task_id >= len(list): #make it a search func
         raise HTTPException(status_code=404, detail="Task not found")
     completed_task = list.pop(task_id)
-    return completed_task
+    write_tasks(list)
+    return completed_task """
+
+
+@app.post("/complete/")
+async def complete_task(task_idd: int):
+    list=get_list()
+    for index, task in enumerate(list):
+        if task.task_id == task_idd:
+        #if task.task_id == task_idd:
+            completed_task = list.pop(index)
+            write_tasks(list)
+            return completed_task
+    
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 @app.get("/delete/")
 async def delete_list():
     global list
     list = []
+    write_tasks(list)
     return list
 
 
--- to_do_list/client.py
+++ to_do_list/client.py
@@ -1,10 +1,8 @@
 import requests
 
-
-
 operation="add"
-new_task_data = {"task_id": 7, "task": "Reading"}
-complete_task_data = {"task_id": 2}
+new_task_data = {"new_task": "sleeping"}
+complete_task_data = {"task_idd": 10}
 
 
 if operation == "list" :
@@ -12,7 +10,7 @@ if operation == "list" :
     
 
 elif operation == "add" :
-    response = requests.post("http://localhost:8000/add/", json=new_task_data)
+    response = requests.post("http://localhost:8000/add/", params=new_task_data)
       
 elif operation == "complete":
     response = requests.post("http://localhost:8000/complete/", params=complete_task_data)
@@ -24,9 +22,7 @@ else:
     print("Invalid operation. Please select from these operations: list, add, complete, delete")
     response=None
 
-
-
-if response :
+if response is not None:
     if response.status_code==200 :
         print(response.json()) 
     else:
